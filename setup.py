ca53120b350c416f52aaea80ced4e6350b674356
ddppddpp <djidji.perroto@gmail.com>
2021-12-21 16:05:51 +0200

    standardized project structure

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
 
 
 setup(
-    name='bgchof',
-    version='0.5.0',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    packages = find:
-
+    name="bgchof",
+    description="(B)ul(g)arian (Ch)ristian (O)rthodox (F)asting",
+    version="0.5.0",
+    long_description=(here / "README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    py_modules=[
+        "bgchof",
+        "calculateEaster",
+        "fastingIO",
+        "fastingStatus",
+        "generateCalendar",
+    ],
+    python_requires=">=3.7, <4",
 )
