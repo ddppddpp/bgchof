d911902222b62f13c299e304768b084a3868979e
ddppddpp <djidji.perroto@gmail.com>
2021-12-24 15:32:32 +0200

    Modified calcEaster(inputYear) to pass all tests

--- src/tests/test_fastingStatus.py
+++ src/tests/test_fastingStatus.py
@@ -1,2 +1,4 @@
-#check page 511, 512 in the Tipikon
-#The rules there are concise enough and can be applied as a series of tests
+# check page 511, 512 in the Tipikon
+# The rules there are concise enough and can be applied as a series of tests
+# probably test for a random series of dates
+# should be something like self.arretIn(getStatusForDate, range(0,6))
