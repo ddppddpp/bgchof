ac585d317025c67f19810788aea7528270b72b4e
ddppddpp <djidji.perroto@gmail.com>
2022-01-03 17:25:46 +0200

    path

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,15 @@
-__version__ = "0.5.4"
+__version__ = "0.5.5"
 __author__ = "Ivailo Djilianov"
+
+
+from .fastingIO import readFastingList, writeFastingList
+from .fastingStatus import fastingValue2Msg, fastingStatusMessage
+from .generateCalendar import (
+    generateList,
+    generateBaseFasting,
+    getVeganDaysm,
+    getVegetarianDays,
+    fastingYearList,
+    yearDayCurrYear,
+)
+from .bgchof import getFastingMessageForDate, getStatusForDate
