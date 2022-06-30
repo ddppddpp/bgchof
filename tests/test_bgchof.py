982a33655a3ed159d03a40392795f2f4a6b387b7
ddppddpp <djidji.perroto@gmail.com>
2022-06-30 15:06:49 +0300

    fixed test for the day of the wholy cross

--- tests/test_bgchof.py
+++ tests/test_bgchof.py
@@ -40,7 +40,7 @@ def test_st_peter_and_pauls_fast_year_2022():
 
 # check status for Sept. 14 - Day of the wholy cross - should be '2'
 def test_status_sept_14_1976_equals_2():
-    assert getStatusForDate(date.fromisoformat("1976-09-14"))
+    assert getStatusForDate(date.fromisoformat("1976-09-14")) == 2
 
 
 # other tests
